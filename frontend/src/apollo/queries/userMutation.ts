b7c692caa08a229855c88b19a9798749df0544b4
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2024-12-03 16:34:38 +0200

    setup apollo

---
+++ frontend/src/apollo/queries/userMutation.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($firstName: String!) {
+    updateUser(firstName: $firstName) {
+      firstName
+      familyName
+    }
+  }
+`;
