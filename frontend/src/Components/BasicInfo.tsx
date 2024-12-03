50d60dc607503741224f14f9c951f5d295c891f7
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2024-12-03 18:03:09 +0200

    add modal

--- frontend/src/Components/BasicInfo.tsx
+++ frontend/src/Components/BasicInfo.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../apollo/queries/userQuery';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import EditBasicInfo from './EditBasicInfo';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 export default function BasicInfo(props:any){
     const {loading,error,data} = useQuery(GET_USER,{
         variables: {userId:1}
     })
     console.log(data,"data");
-    
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return(
         <>
         <div className='bg-white p-5 rounded-lg mb-4'>
             <div className='flex justify-between mb-5'>
             <h1 className='text-2xl font-medium'>Basic Information</h1>
-            <button className='bg-[#003fad] text-white py-2 px-8 rounded'>Edit</button>
+            <button className='bg-[#003fad] text-white py-2 px-8 rounded'  onClick={handleOpen}>Edit</button>
             </div>
             <div className='grid grid-cols-4 gap-6 '>
                 <div>
@@ -111,6 +130,16 @@ export default function BasicInfo(props:any){
                 </div>
         </div>
         </div>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+        <EditBasicInfo/>
+        </div>
+      </Modal>
         </>
     )
 }
